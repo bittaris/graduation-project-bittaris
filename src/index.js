925ad26ac81d2b480c20b04c282f3e13c0829e2c
bittaris <150023736+bittaris@users.noreply.github.com>
2023-12-06 18:00:47 +0000

    I added even more information I want from users during sign up.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 class User {
-  constructor(username, fullName, email, password, birthDate, location, height, weight, currentSymptoms, goals) {
+  constructor(
+    username,
+    fullName,
+    email,
+    password,
+    birthDate,
+    location,
+    height,
+    weight,
+    currentSymptoms,
+    goals,
+    firstDayLastPeriod,
+    usualPeriodLength,
+    usualCycleLength
+  ) {
     this.username = username
     this.fullName = fullName
     this.email = email
@@ -10,6 +24,9 @@ class User {
     this.weight = weight // Same as line above. But for now I'm treating the height as given in cm and the weight in kg. I also want the weight to be a float.
     this.currentSymptoms = currentSymptoms // This is a huge list of options
     this.goals = goals
+    this.firstDayLastPeriod = firstDayLastPeriod // To help determine which phase of the cycle the user is in
+    this.usualPeriodLength = usualPeriodLength // To help establish bleed phase length
+    this.usualCycleLength = usualCycleLength // To help establish when ovulation occurs and situate the other phases
   }
 }
 
@@ -49,7 +66,10 @@ const Penelope = new User( // I added all the symptoms and goals on this user to
     'Improve sleep',
     'Manage stress',
     'Have more energy',
-  ]
+  ],
+  '23.11.23',
+  7,
+  34
 )
 
 const Hannah = new User(
@@ -62,7 +82,10 @@ const Hannah = new User(
   145,
   60,
   ['Weight gain', 'Difficulty losing weight', 'Low energy', 'Sugar / salty cravings'],
-  ['Feel good in my own skin', 'Improve nutrition', 'Improve skin', 'Exercise / move more']
+  ['Feel good in my own skin', 'Improve nutrition', 'Improve skin', 'Exercise / move more'],
+  '16.11.2023',
+  5,
+  28
 )
 
 const Jules = new User(
@@ -75,5 +98,16 @@ const Jules = new User(
   181,
   90,
   ['Difficulty losing weight', 'Low energy', 'Sugar / salty cravings', 'Anxiety', 'Depression', 'Insulin resistance'],
-  ['Exercise / move more', 'Improve sleep', 'Manage stress', 'Have more energy']
+  ['Exercise / move more', 'Improve sleep', 'Manage stress', 'Have more energy'],
+  '05.11.2023',
+  4,
+  32
 )
+
+// Future stuff to implement:
+// Calculate BMI using height and weight
+// Use firstDayLastPeriod, usualPeriodLength, usualCycleLength against the day they sign up to let them know in which predicted phase they are in
+// Create predictions for their upcoming period phases; ovulation, lutheal, follicular and bleed
+// Use their location information to gather weather info and how that might impact the user depending on where they are on their cycle
+// Create an algorithm that suggests readings and daily tips on the topics that are of importance to the user
+// Create an algorithm that suggests exercises, activities and techniques according to users needs
