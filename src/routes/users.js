295798f5984b4535e2a1fce24574a827045afbde
bittaris <150023736+bittaris@users.noreply.github.com>
2023-12-20 09:33:00 +0000

    Push 2 objects to client side to verify connection

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../user')
+const Product = require('../product')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   const bunny = new User('bunny')
+  const bouquetOne = new Product('Bouquet One', '10 Pink Peonies', '30€')
+  bunny.addItem(bouquetOne)
   res.send(bunny)
 })
 
