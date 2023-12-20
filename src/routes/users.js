1384e1a17fffecaa49a3a33bcc245e76f9f04c16
bittaris <150023736+bittaris@users.noreply.github.com>
2023-12-20 09:20:09 +0000

    Push full user to the client

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,7 @@ const User = require('../user')
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   const bunny = new User('bunny')
-  res.send([{ username: bunny.username }])
+  res.send(bunny)
 })
 
 module.exports = router
