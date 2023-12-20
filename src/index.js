ecc43c5a29db6f771f3553c2b471453156e3d4f2
bittaris <150023736+bittaris@users.noreply.github.com>
2023-12-20 09:39:57 +0000

    Rename function to move all of the code into one Main

--- src/index.js
+++ src/index.js
@@ -9,32 +9,31 @@ const client = Axios.create({
   headers: { 'X-Custom-Header': 'foobar' },
 })
 
-async function getUsers() {
+async function main() {
   try {
     const response = await client.get('/users')
     console.log(response.data)
+    const bunny = new User('bunny')
+
+    const bouquetOne = new Product('Bouquet One', '10 Pink Peonies', '30€')
+    const bouquetTwo = new Product('Bouquet Two', '20 Yellow Roses', '30€')
+
+    bunny.addItem(bouquetOne)
+    console.log("Bunny's cart: " + bunny.cart)
+
+    bunny.removeItem(bouquetOne)
+    console.log("Bunny's cart: " + bunny.cart)
+
+    bunny.addItem(bouquetTwo)
+    console.log("Bunny's cart: " + bunny.cart)
+
+    const bunnysOrder = new Order(bunny, bunny.cart, 'Lalastr. 22 12345')
+
+    bunny.placeOrder('Lalastr. 22 12345')
+    console.log("Bunny's order: " + bunny.placeOrder('Lalastr. 22 12345'))
   } catch (error) {
     console.error(error)
   }
 }
 
-getUsers()
-
-const bunny = new User('bunny')
-
-const bouquetOne = new Product('Bouquet One', '10 Pink Peonies', '30€')
-const bouquetTwo = new Product('Bouquet Two', '20 Yellow Roses', '30€')
-
-bunny.addItem(bouquetOne)
-console.log("Bunny's cart: " + bunny.cart)
-
-bunny.removeItem(bouquetOne)
-console.log("Bunny's cart: " + bunny.cart)
-
-bunny.addItem(bouquetTwo)
-console.log("Bunny's cart: " + bunny.cart)
-
-const bunnysOrder = new Order(bunny, bunny.cart, 'Lalastr. 22 12345')
-
-bunny.placeOrder('Lalastr. 22 12345')
-console.log("Bunny's order: " + bunny.placeOrder('Lalastr. 22 12345'))
+main()
