7bcf2b86c2724971c6a1e1766a08912e137335ab
bittaris <150023736+bittaris@users.noreply.github.com>
2023-12-20 13:55:44 +0000

    Separate products and users, fix index.js

--- src/index.js
+++ src/index.js
@@ -11,45 +11,59 @@ const client = Axios.create({
 
 async function main() {
   try {
-    //const usersResponse = await client.get('/users')
-    //console.log(usersResponse.data)
+    const usersResponse = await client.get('/users')
+    console.log(usersResponse.data)
     const productsResponse = await client.get('/products')
 
     const products = productsResponse.data
     console.log(products)
 
-    // const bunny = new User(usersResponse.data.username)
-    // const turtle = new User(usersResponse.data.username)
+    const bunny = new User(usersResponse.data.username)
+    const turtle = new User(usersResponse.data.username)
 
-    // const bunnyUser = usersResponse.data[0]
-    // console.log('bunnyUser: ' + bunnyUser)
-    // const turtleUser = usersResponse.data[1]
-    // console.log('turtleUser: ' + turtleUser)
+    const bunnyUser = usersResponse.data[0]
+    console.log('bunnyUser: ' + bunnyUser)
+    const turtleUser = usersResponse.data[1]
+    console.log('turtleUser: ' + turtleUser)
 
-    // const bouquetOne = productsResponse.data[0]
-    // const bouquetTwo = productsResponse.data[1]
-    // const bouquetThree = productsResponse.data[2]
-    // const bouquetFour = productsResponse.data[3]
-    // const bouquetFive = productsResponse.data[4]
+    const bouquetOne = new Product(
+      productsResponse.data[0].title,
+      productsResponse.data[0].description,
+      productsResponse.data[0].price
+    )
+    const bouquetTwo = new Product(
+      productsResponse.data[1].title,
+      productsResponse.data[1].description,
+      productsResponse.data[1].price
+    )
+    const bouquetThree = new Product(
+      productsResponse.data[2].title,
+      productsResponse.data[2].description,
+      productsResponse.data[2].price
+    )
+    const bouquetFour = new Product(
+      productsResponse.data[3].title,
+      productsResponse.data[3].description,
+      productsResponse.data[3].price
+    )
+    const bouquetFive = new Product(
+      productsResponse.data[4].title,
+      productsResponse.data[4].description,
+      productsResponse.data[4].price
+    )
 
-    // const bouquetOne = new Product(bunnyUser.cart[0].title, bunnyUser.cart[0].description, bunnyUser.cart[0].price)
-    // const bouquetTwo = new Product(bunnyUser.cart[1].title, bunnyUser.cart[1].description, bunnyUser.cart[1].price)
-    // const bouquetThree = new Product(turtleUser.cart[0].title, turtleUser.cart[0].description, turtleUser.cart[0].price)
-    // const bouquetFour = new Product(turtleUser.cart[1].title, turtleUser.cart[1].description, turtleUser.cart[1].price)
-    // const bouquetFive = new Product(turtleUser.cart[2].title, turtleUser.cart[2].description, turtleUser.cart[2].price)
+    bunny.addItem(bouquetOne)
+    bunny.addItem(bouquetTwo)
+    console.log("Bunny's cart: " + bunny.cart.length)
 
-    // bunny.addItem(bouquetOne)
-    // bunny.addItem(bouquetTwo)
-    // console.log("Bunny's cart: " + bunny.cart.length)
+    turtle.addItem(bouquetThree)
+    turtle.addItem(bouquetFour)
+    turtle.addItem(bouquetFive)
+    console.log("Turtle's cart: " + turtle.cart.length)
 
-    // turtle.addItem(bouquetThree)
-    // turtle.addItem(bouquetFour)
-    // turtle.addItem(bouquetFive)
-    // console.log("Turtle's cart: " + turtle.cart.length)
-
-    //const bunnysOrder = new Order(bunny, bunny.cart, 'Lalastr. 22 12345')
-    //bunny.placeOrder('Lalastr. 22 12345')
-    //console.log("Bunny's order: " + bunnysOrder)
+    const bunnysOrder = new Order(bunny, bunny.cart, 'Lalastr. 22 12345')
+    bunny.placeOrder('Lalastr. 22 12345')
+    console.log("Bunny's order: " + bunnysOrder)
   } catch (error) {
     console.error(error)
   }
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,18 +8,6 @@ router.get('/', function (req, res, next) {
   const bunny = new User('bunny')
   const turtle = new User('turtle')
 
-  // const bouquetOne = new Product('Bouquet One', '10 Pink Peonies', '30€')
-  // const bouquetTwo = new Product('Bouquet Two', '20 Yellow Roses', '35€')
-  // const bouquetThree = new Product('Bouquet Three', '25 Pink Carnations', '30€')
-  // const bouquetFour = new Product('Bouquet Four', '15 Yellow Daffodils', '40€')
-  // const bouquetFive = new Product('Bouquet Five', '25 White Roses', '40€')
-
-  bunny.addItem(bouquetOne)
-  bunny.addItem(bouquetTwo)
-  turtle.addItem(bouquetThree)
-  turtle.addItem(bouquetFour)
-  turtle.addItem(bouquetFive)
-
   res.send([bunny, turtle])
 })
 
