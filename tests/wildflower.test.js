de6ba727fd8c21489fe310e7f820e251cc769d3e
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-11 19:02:58 +0000

    Add test for getting all users

--- tests/wildflower.test.js
+++ tests/wildflower.test.js
@@ -16,4 +16,25 @@ describe('Wildflower', () => {
     expect(actualOutput.body).toMatchObject(expectedOutput)
     expect(actualOutput.body._id).toBeDefined()
   })
+  it('can get all users', async () => {
+    const bunny = await request(app).post('/users').send({ username: 'Bunny' })
+    const lula = await request(app).post('/users').send({ username: 'Lula' })
+
+    const expectedOutput = [
+      {
+        username: 'Bunny',
+        cart: [],
+        _id: bunny.body._id,
+      },
+      {
+        username: 'Lula',
+        cart: [],
+        _id: lula.body._id,
+      },
+    ]
+
+    const actualOutput = await request(app).get('/users')
+
+    expect(actualOutput.body).toMatchObject(expectedOutput)
+  })
   })
