134676bebf90b82c0aebb3428b0b611590db0512
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-15 20:11:10 +0000

    Add ProductsView component and axios import

---
+++ frontend/src/views/ProductsView.vue
@@ -0,0 +1,34 @@
+<script setup>
+// composition API
+import axios from 'axios'
+
+const productsResponse = await axios.get('http://localhost:3000/products') // top level await
+
+const products = productsResponse.data
+</script>
+
+<template>
+  <div class="products">
+    <h1>Bouquets</h1>
+    <ul>
+      <li v-for="product in products" :key="product._id">
+        <a :href="`/products/${products._id}`">
+          {{ product.title }}
+        </a>
+        {{ product.description }} for {{ product.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: rgb(248, 246, 243);
+}
+a {
+  color: green;
+}
+</style>
