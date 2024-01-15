9bcbfac5c82bb93f91cead5c44f7bc0310af77d9
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-15 11:03:45 +0000

    Add cors package and enable CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-require('dotenv').config()
+const cors = require('cors')
+
 require('./database-connection')
 
 const indexRouter = require('./routes/index')
@@ -12,6 +15,8 @@ const productsRouter = require('./routes/products')
 const ordersRouter = require('./routes/orders')
 
 const app = express()
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
