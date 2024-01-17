07d0ccecf5a590d1be4ecb8b14d21d8f4eeab903
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-17 09:18:43 +0000

    Create composition Counter

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  margin-top: 1rem;
+  color: rgb(248, 246, 243);
+}
