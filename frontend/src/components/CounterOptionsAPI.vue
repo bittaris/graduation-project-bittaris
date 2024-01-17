ad60e4c8c6a591c1e0d2cccf5b95d9bb18a258b3
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-17 09:19:16 +0000

    Create CounterOptionsAPI component

---
+++ frontend/src/components/CounterOptionsAPI.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
