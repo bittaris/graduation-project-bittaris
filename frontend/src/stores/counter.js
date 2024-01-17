9535b44bc4f14aee2b57cef51904428f3c0176b3
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-17 11:48:34 +0000

    Refactor counter store to contain both/either options and composition api

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+// composition api implementation
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
@@ -13,3 +14,21 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment, decrement }
 })
+
+// options api implementation
+// export const useCounterStore = defineStore('counter', {
+//   state: () => {
+//     return { count: 0 }
+//   },
+//   getters: {
+//     doubleCount: (state) => state.count * 2
+//   },
+//   actions: {
+//     increment() {
+//       this.count++
+//     },
+//     decrement() {
+//       this.count--
+//     }
+//   }
+// })
