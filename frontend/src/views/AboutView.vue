4583e05b1f0cf9e27eec964359ecc5c2965fb2fc
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-24 18:21:34 +0000

    Update AboutView.vue with new content

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>The cutest flower shop you'll never see</h1>
+    <h2>Wildflower is your local online flower shop</h2>
+    <br />
+    <h3>Who are we?</h3>
+    <p>
+      We are a small team of flower lovers (florists and flower farmers) who want to make it easy
+      for you to send flowers to your loved ones. Flowers are a beautiful way to show you care and
+      we know that it can be a hassle to go to a flower shop last minute and pick out the perfect
+      bouquet. That's where we come in to help!
+    </p>
+    <h3>Where's the shop located?</h3>
+    <p>
+      Great question, glad you asked. We are a small collective and our main objective in life is to
+      be around flowers as much as possible. We are also aware of dwindling resources and the fact
+      that the planet it slowly dying, so we decided to be smart about our investments. Instead of
+      having an expensive walk in flower shop (even though we love the idea, maybe for the future),
+      we decided to rent out plots of land on the outskirts of Berlin where we produce our own
+      flowers and keep bee colonies. We want our business to have a positive impact on the
+      environment and we want it to look cute while doing it.
+    </p>
   </div>
 </template>
 
