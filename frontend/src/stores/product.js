60b5f4714e4447abf1c24fd74ed1aed0a2d7cdd1
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-24 18:22:59 +0000

    Set axios defaults for withCredentials and baseURL

--- frontend/src/stores/product.js
+++ frontend/src/stores/product.js
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
+
 // options api implementation
 export const useProductStore = defineStore('product', {
   // state: () => {
