5ca0e41d0dc818600221ab76a326e68858a8167e
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-24 18:21:17 +0000

    Update HomeView.vue with new content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <Suspense>
-      <TheWelcome />
-    </Suspense>
-  </main>
+  <h1>Welcome to Wildflower</h1>
+  <h2>Your local online flower shop</h2>
+  <br />
+  <br />
+  <h3>Wanna be cute and that show you</h3>
+  <h3>care without actually being there?</h3>
+  <a href="/products" class="btn btn-primary">Send flowers</a>
+  <br />
+  <br />
+  <h4>All of the credibility</h4>
+  <h4>none of the hassle</h4>
 </template>
