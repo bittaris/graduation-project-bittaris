fab0be97b72c0d39c3a4dff83d281d5990525df9
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-24 18:39:42 +0000

    Remove commented out code in database-connection.js

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -3,9 +3,3 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log('Connected to MongoDB', process.env.MONGODB_CONNECTION_STRING))
   .catch(error => console.log('Error connecting to MongoDB', error.message ? error.message : error))
-
-// const User = mongoose.model('User', { username: String, cart: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }] })
-
-// const user = new User({ username: 'Bunny', cart: [] })
-
-// user.save().then(() => console.log('User saved'))
