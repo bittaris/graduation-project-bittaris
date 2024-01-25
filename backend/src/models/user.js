474273a0e1bde283139ae2be170b619b9632e925
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-25 18:13:10 +0000

    Add user types to differentiate customer from owner.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,11 +2,13 @@ const Order = require('./order')
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
 const passportLocalMongoose = require('passport-local-mongoose')
+const Product = require('./product')
 
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   cart: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', autopopulate: true }],
+  type: { type: String, enum: ['owner', 'customer'], default: 'customer' },
 })
 
 class User {
@@ -22,6 +24,12 @@ class User {
     let newOrder = await Order.create({ customer: this, items: this.cart, deliveryAddress: deliveryAddress })
     this.cart = []
     return newOrder
+  }
+
+  // Actions only for the owner of the shop
+  // Product creation
+  async createProduct(title, description, price) {
+    return await Product.create({ title, description, price })
   }
 }
 
