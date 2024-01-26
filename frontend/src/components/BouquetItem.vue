7b049859c20d4ba51f1e9e9a695710cbcecdb89e
bittaris <150023736+bittaris@users.noreply.github.com>
2024-01-26 17:05:24 +0000

    Add BouquetItem component and update ProductsView

---
+++ frontend/src/components/BouquetItem.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'BouquetItem',
+  // data() {
+  //   return {
+  //     product: {
+  //       title: 'Bouquet One',
+  //       description: 'Composed of pink peonies available in different sizes.',
+  //       price: '30€',
+  //       image: {
+  //         url: '/images/bouquet1.jpg',
+  //         alt: 'Bouquet composed of 5 pink peonies wrapped in simple white paper'
+  //       }
+  //     }
+  //   }
+  // },
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<template>
+  <div class="products">
+    <div class="card-group">
+      <div class="card">
+        <img
+          src="/images/bouquet1.jpg"
+          class="card-img-top"
+          alt="Bouquet composed of 5 pink peonies wrapped in simple white paper"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ product.title }}</h5>
+          <p class="card-text">{{ product.description }}</p>
+          <a href="#" class="btn btn-primary">Select size</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,6 +1,7 @@
 <script setup>
 // composition API
 import axios from 'axios'
+import BouquetItem from '@/components/BouquetItem.vue'
 
 const productsResponse = await axios.get('/products') // top level await
 
@@ -10,12 +11,15 @@ const products = productsResponse.data
 <template>
   <div class="products">
     <h1>Bouquets</h1>
-    <ul>
-      <li v-for="product in products" :key="product._id">
-        <RouterLink :to="`/products/${product._id}`">{{ product.title }}</RouterLink>
-        {{ product.description }} for {{ product.price }}
-      </li>
-    </ul>
+    <div class="container text-center">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3">
+        <div class="col g-4" v-for="product in products" :key="product._id">
+          <BouquetItem :product="product" />
+          <!-- <RouterLink :to="`/products/${product._id}`">{{ product.title }}</RouterLink>
+        {{ product.description }} for {{ product.price }} -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,5 +33,14 @@ h1 {
 }
 a {
   color: rgb(20, 145, 0);
+}
+.btn {
+  background-color: rgb(20, 145, 0);
+  border-color: rgb(20, 145, 0);
+  color: white;
+}
+
+img {
+  height: 300px;
 }
 </style>
