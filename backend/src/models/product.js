7ee1fc744ff2cb63e89cbba4241514a37603b5e7
bittaris <150023736+bittaris@users.noreply.github.com>
2024-02-01 15:48:32 +0000

    Add autopopulate plugin to productSchema for the image.

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 
 const productSchema = new mongoose.Schema({
   title: String,
@@ -7,9 +8,11 @@ const productSchema = new mongoose.Schema({
   image: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Image',
+    autopopulate: true,
   },
 })
 
 class Product {}
 
+productSchema.plugin(autopopulate)
 module.exports = mongoose.model('Product', productSchema)
