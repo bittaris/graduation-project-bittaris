52d183b6b3c9bb1aedb9b290a18b510e482329f3
bittaris <150023736+bittaris@users.noreply.github.com>
2024-02-15 09:07:33 +0000

    Implement adding and subtracting item amount and item deletion from the cart locally and on the server.

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -14,45 +14,94 @@ export default {
     ...mapState(useAccountStore, ['user'])
   },
   methods: {
-    ...mapActions(useUserStore, ['fetchCart', 'removeItemFromCart']),
-    removeItem(productId) {
-      this.removeItemFromCart(this.user._id, productId, 1)
+    ...mapActions(useUserStore, ['fetchCart', 'addItemToCart', 'removeItemFromCart']),
+    async addItem(productId) {
+      // this.addItemToCart(this.user._id, productId, 1)
+      let item = this.cart.find((item) => item.product._id === productId)
+      if (item) {
+        item.quantity++
+      }
+      // Save the updated cart to local storage
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+      // Update cart on the server
+      await this.addItemToCart(this.user._id, productId, 1)
+    },
+
+    async removeItem(productId) {
+      // this.removeItemFromCart(this.user._id, productId, 1)
+      let item = this.cart.find((item) => item.product._id === productId)
+      if (item) {
+        item.quantity--
+        if (item.quantity === 0) {
+          this.cart = this.cart.filter((item) => item.product._id !== productId)
+        }
+      }
+      // Save the updated cart to local storage
+      localStorage.setItem('cart', JSON.stringify(this.cart))
+      // Update cart on the server
+      await this.removeItemFromCart(this.user._id, productId, 1)
     }
   },
-
   async mounted() {
     this.cart = await this.fetchCart(this.user._id)
+  },
+  created() {
+    let savedCart = localStorage.getItem('cart')
+    if (savedCart) {
+      this.cart = JSON.parse(savedCart)
+    }
   }
 }
 </script>
 <template>
-  <div class="cart">
-    <h1>Cart</h1>
-    <div class="container text-center">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3">
-        <div class="col g-4" v-for="item in cart" :key="item.product._id">
-          <div class="card">
-            <img :src="item.product.image" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">{{ item.product.title }}</h5>
-              <p class="card-text">{{ item.product.description }}</p>
-              <p class="card-text">{{ item.product.price }}</p>
-              <p class="card-text">{{ item.quantity }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <RouterLink class="btn btn-primary" to="/checkout">Proceed to checkout</RouterLink>
+  <h1>Cart</h1>
+  <div v-for="item in cart" :key="item.product._id">
+    <div class="item-body">
+      <h2 class="item-title">{{ item.product.title }}</h2>
+      <h3 class="item-text">{{ item.product.description }}</h3>
+      <p class="item-text">{{ item.product.price }}</p>
+      <p class="item-text">
+        <i @click="addItem(item.product._id)" type="button" aria-label="Plus">
+          <i class="bi bi-plus-circle"></i>
+        </i>
+        {{ item.quantity }}
+        <i @click="removeItem(item.product._id)" type="button" aria-label="Minus">
+          <i class="bi bi-dash-circle"></i>
+        </i>
+      </p>
+      <p>---------------------------------------</p>
     </div>
   </div>
+  <RouterLink class="btn btn-primary" to="/checkout">Proceed to checkout</RouterLink>
 </template>
-<style scope>
+<style scoped>
 h1 {
   font-size: 2rem;
   margin: 0 0 0 3rem;
+  color: whitesmoke;
+}
+h2 {
+  font-size: 1.5rem;
+  /* margin-bottom: 0.7rem; */
+  color: whitesmoke;
+}
+h3 {
+  font-size: 1.15rem;
+  /* margin-bottom: 0.7rem; */
+  color: rgb(224, 224, 224);
+}
+p {
+  font-size: 1rem;
+  /* margin-bottom: 0.7rem; */
+  color: rgb(224, 224, 224);
+}
+.item-body {
+  margin: 2rem 0 2rem 3rem;
 }
 .btn-primary {
-  margin: 2rem 0rem 5rem 0rem;
-  padding: 0.7rem 1.5rem 0.7rem 1.5rem;
+  background-color: rgb(8, 62, 0);
+  border-color: rgb(8, 62, 0);
+  color: white;
+  margin-left: 6.5rem;
 }
 </style>
